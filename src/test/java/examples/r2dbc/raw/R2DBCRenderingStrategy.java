31babf70a5e5d88e243dbd666d66a45bb6b83476
Jeff Butler <jeffgbutler@gmail.com>
2023-09-07 13:02:57 -0400

    Docs of different binding strategies

--- src/test/java/examples/r2dbc/raw/R2DBCRenderingStrategy.java
+++ src/test/java/examples/r2dbc/raw/R2DBCRenderingStrategy.java
@@ -7,6 +7,16 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class R2DBCRenderingStrategy extends RenderingStrategy {
 
+    // see here for how Spring figures this out:
+    // https://github.com/spring-projects/spring-data-relational/blob/main/spring-data-r2dbc/src/main/java/org/springframework/data/r2dbc/dialect/DialectResolver.java
+
+    // H2 (Indexed): $1, $2, $3, ... in statement and parameter map
+    // MySQL (Anonymous): ?, ?, ? ... in statement, must bind by index
+    // Oracle (Named): :P1, :P2, :P3, ... in statement, P1, P2, P3 ... in parameter map
+    // Postgres (Indexed): $1, $2, $3, ... in statement and parameter map
+    // SQL Server (Named): @P1, @P2, @P3... in statement, P1, P2, P3... in parameter map
+
+
     @Override
     public String formatParameterMapKey(AtomicInteger sequence) {
         return "$" + sequence.getAndIncrement(); //$NON-NLS-1$
