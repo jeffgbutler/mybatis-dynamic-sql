689fe13bfd242347abc34c4a37cf8e6bafbba4c1
Jeff Butler <jeffgbutler@gmail.com>
2026-02-17 16:07:13 -0500

    Sonar Nag

--- src/main/java/org/mybatis/dynamic/sql/select/SelectDSL.java
+++ src/main/java/org/mybatis/dynamic/sql/select/SelectDSL.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2016-2025 the original author or authors.
+ *    Copyright 2016-2026 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -43,6 +43,7 @@ import org.mybatis.dynamic.sql.util.Validator;
  *            the type of model produced by this builder, typically SelectModel
  */
 public class SelectDSL<R> implements Buildable<R>, ConfigurableStatement<SelectDSL<R>> {
+    private final static String ERROR_48 = "ERROR.48";
 
     private final Function<SelectModel, R> adapterFunction;
     private final List<QueryExpressionDSL<R>> queryExpressions = new ArrayList<>();
@@ -138,25 +139,25 @@ public class SelectDSL<R> implements Buildable<R>, ConfigurableStatement<SelectD
     }
 
     public SelectDSL<R> forUpdate() {
-        Validator.assertNull(forClause, "ERROR.48"); //$NON-NLS-1$
+        Validator.assertNull(forClause, ERROR_48);
         forClause = "for update"; //$NON-NLS-1$
         return this;
     }
 
     public SelectDSL<R> forNoKeyUpdate() {
-        Validator.assertNull(forClause, "ERROR.48"); //$NON-NLS-1$
+        Validator.assertNull(forClause, ERROR_48);
         forClause = "for no key update"; //$NON-NLS-1$
         return this;
     }
 
     public SelectDSL<R> forShare() {
-        Validator.assertNull(forClause, "ERROR.48"); //$NON-NLS-1$
+        Validator.assertNull(forClause, ERROR_48);
         forClause = "for share"; //$NON-NLS-1$
         return this;
     }
 
     public SelectDSL<R> forKeyShare() {
-        Validator.assertNull(forClause, "ERROR.48"); //$NON-NLS-1$
+        Validator.assertNull(forClause, ERROR_48);
         forClause = "for key share"; //$NON-NLS-1$
         return this;
     }
